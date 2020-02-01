850f2ee308ca6cdc28eb43c714e040734d7cfa09
Cristian Sanchez <crisey937@gmail.com>
2020-01-31 19:08:51 -0500

    added gmaps to the solution

---
+++ FlightsStats/Interface.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlightsStats
+{
+    public partial class Interface : Form
+    {
+        public Interface()
+        {
+            InitializeComponent();
+        }
+
+        private void gMapControl1_Load(object sender, EventArgs e)
+        {
+            gmap.MapProvider = GMap.NET.MapProviders.BingMapProvider.Instance;
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerOnly;
+            gmap.Position = new GMap.NET.PointLatLng(39.680269, -97.723161);
+            gmap.ShowCenter = false;
+        }
+    }
+}
